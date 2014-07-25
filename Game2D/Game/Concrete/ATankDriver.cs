d0b02734afc508a5b088a9453f45823ab325f595
Alexandr <kolesov.alex.s@gmail.com>
2014-07-25 22:40:32 +0400

    simple motion now working

--- Game2D/Game/Concrete/ATankDriver.cs
+++ Game2D/Game/Concrete/ATankDriver.cs
@@ -10,19 +10,64 @@ namespace Game2D.Game.Concrete
 {
     class ATankDriver
     {
+        bool tankPosInited;
+        Point2 destinationPiont;
+
         public ATankDriver()
         {
-
+            tankPosInited = false;
         }
 
         public void Process(List<Command> serverCommands, DStateMain sceneState, ref Frame resultFrame, IGetKeyboardState keyboardState)
         {
             if (sceneState.battle != null && sceneState.battle.me != null)
             {
-                sceneState.battle.me.tank.body.pos = new Vector2(0, 0, 0);
-                sceneState.battle.me.tank.body.Draw(resultFrame);
+                DTankBody tank = sceneState.battle.me.tank.body;
+
+                if (!tankPosInited) //initializating tank position
+                {
+                    tank.pos = new Vector2(0, 0, 0);
+                    destinationPiont = new Point2(0, 0);
+                    tankPosInited = true;
+                }
+
+                if (keyboardState.MouseRightClick)  //new destination point
+                {
+                    destinationPiont = keyboardState.MousePosMap;
+                }
+
+                moveTaktic1(tank);
+
+                //drawing
+
+                resultFrame.Add(new Sprite(tank.sprite, tank.pos, tank.size.x, tank.size.y));
+                resultFrame.Add(new Sprite(tank.sprite, new Vector2(destinationPiont), tank.size.x, tank.size.y));
             }
         }
 
+        void moveTaktic1(DTankBody tank)
+        {
+            if (tank.pos.point.DistTo(destinationPiont) == 0)    //points are similar
+                return;
+
+            if (tank.pos.point.DistTo(destinationPiont) < tank.speed)
+            {
+                tank.pos = new Vector2(destinationPiont, tank.pos.vector);
+                return;
+            }
+
+            Vector2 newPos = new Vector2(0, 0, 0);
+
+            double dx = destinationPiont.x - tank.pos.x;
+            double dy = destinationPiont.y - tank.pos.y;
+            double gyp = Math.Pow(Math.Pow(dx, 2) + Math.Pow(dy, 2), 1/2);
+            double sin = dy / gyp;
+            double cos = dx / gyp;
+
+            newPos.x = tank.pos.x + tank.speed * cos;
+            newPos.y = tank.pos.y + tank.speed * sin;
+
+            tank.pos = newPos;
+        }
     }
 }
--- Game2D/Game/DataClasses/TankParts/DTankBody.cs
+++ Game2D/Game/DataClasses/TankParts/DTankBody.cs
@@ -10,8 +10,8 @@ namespace Game2D.Game.DataClasses
         public Vector2 pos;
         public double speed = 0.1; // сколько проедет за кадр
         public Point2? aimPoint = null; //куда едем, известно только для подконтрольных юнитов
-        Point2 size = new Point2(10, 5);
-        ESprite sprite;
+        public Point2 size = new Point2(10, 5);
+        public ESprite sprite;
         public DTankBody(Vector2 startPos, ESprite sprite)
         {
             this.pos = startPos;
